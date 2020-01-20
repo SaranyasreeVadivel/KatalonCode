3ad63bc6d8d0f52d148bed20266336d6b5e86aee
Sara <sara@creadits.com>
2020-01-20 21:14:51 +0800

    Basic Test case of Login & Open Deck list

---
+++ Test Suites/Login_Admin_Platform.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Admin_Platform</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>daca4814-16d3-4bc5-a310-3e2207c3b540</testSuiteGuid>
+</TestSuiteEntity>
